b6dcc83bec957b85302f4bb74a565a565add0ce7
kira <ab@mkka,com>
2021-01-30 14:31:02 +0300

    first commit

--- RubyScraper/GetDataFromGithub.rb
+++ RubyScraper/GetDataFromGithub.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
+# Libraries
 require 'httparty'
 require 'nokogiri'
 require 'terminal-table'
 
-url = "https://github.com/kira-Developer?tab=repositories"
+url = 'https://github.com/kira-Developer?tab=repositories'
 
-response = HTTParty::get url
+response = HTTParty.get url
 if response.code == 200
-  doc = Nokogiri::HTML::DocumentFragment.parse response.body
-  a = doc::css 'h3.wb-break-all a'
-   aHref = doc::css 'h3.wb-break-all a[href]'
-  table = Terminal::Table.new :headings  =>['lang' ,'' , 'url'] , :style => {}  do |t|
-   aHref.map {|href| href}.each do |links|
-     arr = [links.text.split("\n").join('') , " =>" , " https://github.com" + links['href']]
-     t.add_row arr
-     t.add_separator
+  doc = Nokogiri::HTML::DocumentFragment.parse response.body # Get response Body
+  a = doc.css 'h3.wb-break-all a[href]' # The Tag I Want Find It
+  table = Terminal::Table.new headings: ['lang', '', 'url'], style: {} do |t| # Terminal Table
+    a.map { |href| href }.each do |links| # Searching For Text And Links
+      arr = [links.text.split("\n").join(''), ' =>', " https://github.com#{links['href']}"] # Put The Data In Array
+      t.add_row arr # Put Array In Table Rows
+      t.add_separator # Add Separator
+    end # End map Each
+  end # End Table Each
+end # End The Condition
 
-   end
-  end
-end
-table.style = {:width => 90, :padding_left => 3, :border_x => "=", :border_i => "x"}
+table.style = { width: 90, padding_left: 3, border_x: '=', border_i: 'x' } # Set Some Design
 puts table
